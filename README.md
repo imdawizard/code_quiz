0aaa1a4737b0377f3bca606b7429ffed4a12f140
Spyrodragonman Mannyman <nate.is.cool.yeah@gmail.com>
2023-05-14 19:32:48 -0600

    needs score page to be saved and added to local storage

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # code_quiz
 The project for week 4, a quiz of coding
+
+
+
+##Overview this creates a quiz that pops questions on the screen and then proceeds through qestions until at the end the scores are displayed
--- assets/script.js
+++ assets/script.js
@@ -18,25 +18,29 @@ var questions = [
 ];
 
 var currentQuestionIndex = 0;
-var time = questions.length * 15;
+var time = 60;
 var timerId;
 
 // Get references to DOM elements
 var startButtonEl = document.querySelector("#start-button");
-var questionContainerEl = document.querySelector("#question-container");
 var questionEl = document.querySelector("#question");
 var choicesEl = document.querySelector("#choices");
 var feedbackEl = document.querySelector("#feedback");
-var timeEl = document.querySelector("#time");
+var timeEl = document.querySelector("#time-remaining");
+var questionPageE1 = document.querySelector('#quiz-page');
+var endbtnEl = document.querySelector("#End-button");
+var submitbtn = document.querySelector("#submit-button");
+var scores = [""];
+// var scores = localStorage.getItem("scores");
+
+// var scores = localStorage.getItem
 
 // Function to start the quiz
 function startQuiz() {
   // Hide the start button
   startButtonEl.classList.add("hide");
-
   // Show the question container
-  questionContainerEl.classList.remove("hide");
-
+    questionPageE1.classList.remove("hide");
   // Start the timer
   timerId = setInterval(clockTick, 1000);
 
@@ -73,11 +77,13 @@ function checkAnswer() {
 
   // Check if the answer is correct
   if (userAnswer === questions[currentQuestionIndex].answer) {
+    feedbackEl.classList.remove("hide");
     feedbackEl.textContent = "Correct!";
   } else {
-    feedbackEl.textContent = "Wrong!";
-    time -= 15;
+    time -= 10;
     timeEl.textContent = time;
+    feedbackEl.classList.remove("hide");
+    feedbackEl.textContent = "Wrong!";
   }
 
   // Move to the next question
@@ -97,16 +103,32 @@ function endQuiz() {
   clearInterval(timerId);
 
   // Hide the question container
-  questionContainerEl.classList.add("hide");
+  questionPageE1.classList.add("hide");
 
   // Show the end screen
-  var endScreenEl = document.querySelector("#end-screen");
+  var endScreenEl = document.querySelector("#end-page");
   endScreenEl.classList.remove("hide");
 
   // Display the final score
-  var scoreEl = document.querySelector("#score");
+  var scoreEl = document.querySelector("#final-score");
   scoreEl.textContent = time;
+  scores.unshift(scoreEl);
+  console.log(scores);
+  localStorage.setItem('scores', scores);
 }
+
+function displayScores() {
+    console.log("work?")
+    startQuiz();
+    // var startpageEl = document.querySelector('#start-page');
+    // startpageEl.classList.add("hide");
+    // console.log("work?");
+    // //diplay scores to screen
+    // var scorePageEl = document.querySelector("#score-page")
+    // scorePageEl.classList.remove("hide");
+    // console.log("work again?");
+}
+
 
 // Function to update the timer
 function clockTick() {
@@ -121,4 +143,6 @@ function clockTick() {
 }
 
 // Add event listener for the start button
-startButtonEl.addEventListener("click", startQuiz);
+startButtonEl.addEventListener("click", () => { console.log("sup"); startQuiz(); } );
+endbtnEl.addEventListener("click", () =>  endQuiz() );
+submitbtn.addEventListener("click", displayScores);
